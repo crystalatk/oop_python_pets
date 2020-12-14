a6892f35293d76b4607f425e59d35712859c2161
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2020-12-14 15:40:14 -0500

    Step 1: Pet class and methods created

--- pets.py
+++ pets.py
@@ -1,45 +1,24 @@
-# Define a dictionary that holds our pet's attributes.
-puppy = {
-    "name": "Cujo",
-    "fullness": 50,
-    "happiness": 20,
-}
+class Pet:
+    def __init__(self, name, fullness=50, happiness=50, hunger=5, mopiness=5):
+        self.name = name
+        self.fullness = fullness
+        self.happiness = happiness
+        self.hunger = hunger
+        self.mopiness = mopiness
 
-# Define functions that increase a pet's attribute levels.
-def feed_pet(pet):
-    pet["fullness"] += 10
+    def eat_food(self):
+        self.fullness += 30
 
+    def get_love(self):
+        self.happiness += 30
 
-def play_with_pet(pet):
-    pet["happiness"] += 10
+    def be_alive(self, hunger, mopiness):
+        self.fullness -= self.hunger
+        self.happiness -= self.mopiness
 
-# Decrease a pet's attribute levels.
-def get_hungry_and_mopey(pet):
-    pet["fullness"] -= 5
-    pet["happiness"] -= 5
-
-
-# Prompt the user to interact with the pet
-while True:
-
-    print("""
-%s's stats:
-Fullness: %d
-Happiness: %d
-""" % (puppy["name"], puppy["fullness"], puppy["happiness"]))
-
-    choice = int(input("""
-1. Feed puppy
-2. Play with puppy
-3. Do nothing
-"""))
-    if choice == 1:
-        feed_pet(puppy)
-    elif choice == 2:
-        play_with_pet(puppy)
-    else:
-        pass
-
-    # Each time the loop runs, the pet
-    # will need some attention!
-    get_hungry_and_mopey(puppy)
+    def __str__(self):
+        return '''
+        %s:
+        Fullness: %d
+        Happiness: %d
+        ''' % (self.name, self.fullness, self.happiness)
