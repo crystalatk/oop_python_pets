dc15606a3cc335491a12c9428a872f6f85c23944
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2020-12-14 15:42:29 -0500

    Step 2: create subclass CuddlePet

--- pets.py
+++ pets.py
@@ -22,3 +22,19 @@ class Pet:
         Fullness: %d
         Happiness: %d
         ''' % (self.name, self.fullness, self.happiness)
+
+
+class CuddlyPet(Pet):
+    def __init__(self, name, fullness=50, hunger=5):
+        self.name = name
+        self.fullness = fullness
+        self.happiness = 100
+        self.hunger = hunger
+        self.mopiness = 1
+
+    def be_alive(self):
+        self.fullness -= self.hunger
+        self.happiness -= self.mopiness/2
+
+    def cuddle(self, other_pet):
+        other_pet.get_love()
